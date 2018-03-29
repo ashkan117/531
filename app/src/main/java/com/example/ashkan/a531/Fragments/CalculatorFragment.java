a21b1c31ee170e30c9b0bb7e05e275406366e5d2
ashkan117 <ashkan117@yahoo.com>
2018-03-29 12:24:57 -0700

    trying to pull back

--- app/src/main/java/com/example/ashkan/a531/Fragments/CalculatorFragment.java
+++ app/src/main/java/com/example/ashkan/a531/Fragments/CalculatorFragment.java
@@ -1,6 +1,5 @@
 package com.example.ashkan.a531.Fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
